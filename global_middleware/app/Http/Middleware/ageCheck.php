280cf3a4c5d7715bd6425e15bc68fbb91d772210
Kyaw Linn Tun <kyawlinntun100@gmail.com>
2020-10-24 16:34:51 +0630

    learning global middleware

---
+++ global_middleware/app/Http/Middleware/ageCheck.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class ageCheck
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        // echo 'Hello global middleware';
+        if($request->age && $request->age < 18)
+        {
+            return redirect('noaccess');
+        }
+        return $next($request);
+    }
+}
---
+++ global_middleware/resources/views/noaccess.blade.php
@@ -0,0 +1,1 @@
+<h1>Your age is not accessed</h1>
